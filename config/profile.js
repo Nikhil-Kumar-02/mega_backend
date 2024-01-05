ed45de237d30d9c8ef50887799ff1f6649de913a
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-01-05 19:15:39 +0530

    created profile schema

---
+++ config/profile.js
@@ -0,0 +1,27 @@
+const {Schema,model} = require("mongoose");
+  
+const MySchema = new Schema({
+    gender : {
+        type :String,
+        enum : ["Male" , "Female" , "Others"],
+        required : true,
+        trim : true
+    },
+    dob : {
+        type :String,
+        trim : true
+    },
+    about : {
+        type : String,
+        required : true,
+        trim : true
+    },
+    contactNumber : {
+        type : String,
+        required : true,
+        trim : true
+    },
+});
+
+module.exports = model("profile", MySchema);
+  
