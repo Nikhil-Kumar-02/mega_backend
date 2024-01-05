cc53b7b0e4631cbbf43e3604331f7aa7568810ce
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-01-05 18:59:57 +0530

    created the incoices schema

---
+++ config/invoices.js
@@ -0,0 +1,36 @@
+const {Schema,model} = require("mongoose");
+const mongoose = require('mongoose')
+  
+const MySchema = new Schema({
+    users : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'user'
+    },
+    courseName : {
+        type :String,
+        required : true,
+        trim : true
+    },
+    price : {
+        type : String,
+        required : true,
+        trim : true
+    },
+    address : {
+        type : String,
+        required : true,
+        trim : true
+    },
+    pinCode : {
+        type : String,
+        required : true,
+        trim : true
+    },
+    courseId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'courses'
+    }
+});
+
+module.exports = model("invoices", MySchema);
+  
