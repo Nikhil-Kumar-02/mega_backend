55b792e6329d122f50fc65e66cc86ab1a39c3f3f
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-01-06 07:52:39 +0530

    authorization and authentication basic boiler plate

---
+++ controller/auth.js
@@ -0,0 +1,16 @@
+//send otp as to create account we have to verify the mail
+
+
+
+
+//signup
+
+
+//login
+
+
+
+//this is when you know the current password and still want to change it to new password due to 
+//security reasons
+
+//change password
---
+++ controller/resetPassword.js
@@ -0,0 +1,8 @@
+//this is when you forgot the password and now you want to add new password but you dont know the last pass
+
+//reset password token that is this will be responsible to sending the reset webpage link
+
+
+
+
+//reset password into db
