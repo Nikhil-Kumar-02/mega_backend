900ba4b9b82f88b8e54509e1779b79a1ea243be1
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-01-07 08:06:38 +0530

    wrote logic for resetting password also found one flaw in it

--- controller/resetPassword.js
+++ controller/resetPassword.js
@@ -1,7 +1,6 @@
 //this is when you forgot the password and now you want to add new password but you dont know the last pass
 const { StatusCodes } = require('http-status-codes');
 const User = require('../model/user');
-const jwt = require('jsonwebtoken');
 const sendEmail = require('../utils/mailer');
 
 //reset password token that is this will be responsible to sending the reset webpage link
@@ -54,6 +53,60 @@ const resetPasswordToken = async (req,res) => {
 
 }
 
-
-
 //reset password into db
+const resetPassword = async (req,res) => {
+    //extract the token from the url user visited
+    //this will hitted cuz route will be /api/vi/resetPassword/:token
+    //so we can easily extract the token using the params
+    try {
+        //extract the email newpassword , confirmNewPassword
+        const {password , confirmPassword , token } = req.body;
+        //token is in the body cuz frontend put it there that how babbar used it
+        //validation on inputs recieved
+        if(password != confirmPassword){
+            console.log('password not equal');
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                message : "password not equal",
+                description : "both password and confirm password should be same"
+            })
+        }
+        //get user details from token as now token is also a part of user schema
+        const userDetails = await User.findOne(token);
+
+        //find user and check if the user has this token within the expiry
+        if(!userDetails){
+            console.log('token not found');
+            res.status(StatusCodes.BAD_REQUEST).json({
+                message : "token was not found"
+            }) 
+        }
+        if(userDetails.resetPasswordExpires < Date.now()){
+            //means the token has expired that why we are ahead of time
+            console.log('current time exceeds the token valid time');
+            res.status(StatusCodes.GATEWAY_TIMEOUT).json({
+                message : 'current time exceeds the token valid time',
+                description : "the password reset should be done within 5 mins of link generation"
+            })
+        }
+
+        //if everything goes right update the password
+        const updatedDetails = await User.findOneAndUpdate(token , password , {new :true});
+        res.status(StatusCodes.OK).json({
+            message : 'your password has been sucessfully updated',
+        })
+
+    } catch (error) {
+        console.log('error in controller -> resetpassword.js -> resetpassword');
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            message : "error while trying to reset the password"
+        })
+    }
+    //i can see one problem here we are changing the user details on the basis of url 
+    //so if url is sniffed then someone else can hit the url before token expiration and change
+    //his password before him as we are only finding the user from token and if found then updating the pass
+}
+
+module.exports = {
+    resetPasswordToken , 
+    resetPassword
+}
