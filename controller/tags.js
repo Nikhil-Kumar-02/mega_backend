1bf8a76d2df9e6f581149764153b017274dffcdc
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-01-20 08:34:31 +0530

    checked the tags creating and fetching logic

--- controller/tags.js
+++ controller/tags.js
@@ -9,20 +9,20 @@ const createTag = async (req,res) => {
 
         if(!name || !description){
             console.log('all feilds not present');
-            res.status(StatusCodes.PARTIAL_CONTENT).json({
+            return res.status(StatusCodes.PARTIAL_CONTENT).json({
                 message : 'both name and description is reauired to create a tage'
             })
         }
 
         //both feilds present so create a tag
-        const tagDetails = await Tag.create(name , description);
+        const tagDetails = await Tag.create({name , description});
         console.log(tagDetails);
-        res.status(StatusCodes.OK).json({
+        return res.status(StatusCodes.OK).json({
             message : 'tag created sucessfully'
         })
     } catch (error) {
         console.log('error in controller while creating tag',error)
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
             message : 'cannot create tag',
             error : error
         })
@@ -34,13 +34,13 @@ const showAllTags = async (req,res) => {
     try {
         //we just have to fetch and return all tags
         const tags = await Tag.find({} , {name : true , description:true});
-        res.status(StatusCodes.OK).json({
+        return res.status(StatusCodes.OK).json({
             message : 'sucessfully fetched all tags from db',
             data : tags
         })
     } catch (error) {
         console.log('error in fetching all tags' , error);
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
             message : 'cannot fetch all tags',
             error : error
         })
--- routes/Course.js
+++ routes/Course.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const {createCategory , showAllCategories} = require('../controller/category');
-const {auth , isAdmin} = require('../middleware/auth');
+const {createCourse} = require('../controller/course')
+const {createTag , showAllTags} = require('../controller/tags');
+const {auth , isAdmin, isInstructor} = require('../middleware/auth');
 const router = express.Router();
 
-router.post('/createCategory' ,auth , isAdmin , createCategory)
+router.post('/createCategory' ,auth , isAdmin , createCategory);
 router.get('/showAllCategories' , showAllCategories)
+
+// router.post('/createCourse' ,auth , isInstructor , createCourse)
+
+router.post('/createTag' , auth , isInstructor , createTag);
+router.get('/showAllTags' , showAllTags)
+
+
 
 module.exports = router;
